1dbc13e960f4b239173dfd8020ca379aef06a252
Karoly Hornyak <karoly.webdev@gmail.com>
2026-02-04 21:21:39 +0000

    Updating Header style

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import "./Header.css";
+import { TiShoppingCart as CartIcon } from "react-icons/ti";
+import { FaUser as UserIcon } from "react-icons/fa6";
+import { IoIosSearch as SearchIcon } from "react-icons/io";
 
 export default function Header() {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleSearchIconClick = () => {
+        inputRef.current?.focus();
+    };
+
     return (
         <header className="siteHeader">
             <div className="headerInner">
@@ -21,30 +33,30 @@ export default function Header() {
                     </Link>
 
                     <div className="searchBox">
-                        <span className="searchIcon">⌕</span>
-                        <input type="text" placeholder="Search..." />
+                        {/* ✅ click focus */}
+                        <button
+                            className="searchIconBtn"
+                            onClick={handleSearchIconClick}
+                            aria-label="Focus search"
+                            type="button"
+                        >
+                            <SearchIcon />
+                        </button>
+
+                        <input ref={inputRef} type="text" placeholder="Search..." />
                     </div>
                 </div>
 
                 {/* Center */}
                 <nav className="headerNav">
                     <Link href="/shop" className="navItem active">
-                        Store
-                    </Link>
-                    <Link href="/donate" className="navItem">
-                        Donate Toys
+                        Shop
                     </Link>
                     <Link href="/about" className="navItem">
                         About Us
                     </Link>
                     <Link href="/contact" className="navItem">
                         Reach us
-                    </Link>
-                    <Link href="/loved-again" className="navItem">
-                        Loved Again
-                    </Link>
-                    <Link href="/learn" className="navItem">
-                        Learn
                     </Link>
                 </nav>
 
@@ -57,18 +69,21 @@ export default function Header() {
                     </select>
 
                     <Link href="/cart" className="iconBtn" aria-label="Cart">
-                        <span className="icon">🛒</span>
+                        <span className="icon">
+                            <CartIcon />
+                        </span>
                         <span className="cartCount">0</span>
                     </Link>
 
                     <Link href="/login" className="loginBtn">
-                        <span className="icon">👤</span>
+                        <span className="icon">
+                            <UserIcon />
+                        </span>
                         <span>Log In</span>
                     </Link>
                 </div>
             </div>
 
-            {/* Bottom accent line */}
             <div className="headerAccent" />
         </header>
     );
